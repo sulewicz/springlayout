a30f7faadd8f2d74c3a4a8a5fe7fc7afa38e28ef
Szymon Ulewicz <szymon.ulewicz@getbase.com>
2014-03-11 10:56:14 +0100

    Added nice demo activity.

--- sample/src/org/coderoller/springlayoutsample/MainActivity.java
+++ sample/src/org/coderoller/springlayoutsample/MainActivity.java
@@ -1,10 +1,15 @@
 package org.coderoller.springlayoutsample;
 
+import org.coderoller.springlayout.SpringLayout;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 
 public class MainActivity extends Activity {
   
@@ -12,11 +17,21 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    RelativeWidthChangeAnim aAnimation = new RelativeWidthChangeAnim(findViewById(R.id.A), 10, 50);
+    aAnimation.setDuration(500);
+    aAnimation.setRepeatCount(Animation.INFINITE);
+    aAnimation.setRepeatMode(Animation.REVERSE);
+    findViewById(R.id.A).startAnimation(aAnimation);
+    
+    RelativeWidthChangeAnim bAnimation = new RelativeWidthChangeAnim(findViewById(R.id.B), 50, 10);
+    bAnimation.setDuration(1000);
+    bAnimation.setRepeatCount(Animation.INFINITE);
+    bAnimation.setRepeatMode(Animation.REVERSE);
+    findViewById(R.id.B).startAnimation(bAnimation);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
     return true;
   }
@@ -31,4 +46,30 @@ public class MainActivity extends Activity {
       return super.onOptionsItemSelected(item);
     }
   }
+}
+
+class RelativeWidthChangeAnim extends Animation {
+    View mView;
+    SpringLayout.LayoutParams mLayoutParams;
+    private int mFrom, mTo;
+    
+    public RelativeWidthChangeAnim(View view, int from, int to) {
+        mView = view;
+        mLayoutParams = (SpringLayout.LayoutParams) view.getLayoutParams();
+        mFrom = from;
+        mTo = to;
+    }
+    
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        int relativeWidth;
+        relativeWidth = (int) (mFrom + interpolatedTime * (mTo - mFrom));
+        mLayoutParams.setRelativeWidth(relativeWidth);
+        mView.requestLayout();
+    }
+
+    @Override
+    public boolean willChangeBounds() {
+        return true;
+    }
 }
