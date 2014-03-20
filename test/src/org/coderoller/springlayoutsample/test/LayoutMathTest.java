3be34847c90b8161dac774f21553da55dfd09400
Szymon Ulewicz <szymon.ulewicz@getbase.com>
2014-03-20 23:27:09 +0100

    Fixed unit tests.

--- test/src/org/coderoller/springlayoutsample/test/LayoutMathTest.java
+++ test/src/org/coderoller/springlayoutsample/test/LayoutMathTest.java
@@ -13,62 +13,57 @@ public class LayoutMathTest extends TestCase {
     }
     
     public void testVariableCreation() {
-        LayoutMath.Value v = mTestMath.variable(1);
-        assertEquals(0, v.mRetainCount);
+        LayoutMath.Value v = mTestMath.variable(1).retain();
+        assertEquals(1, v.mRetainCount);
         assertEquals(0, mTestMath.getVariablePoolSize());
-        v.mRetainCount++;
         v.release();
         assertEquals(0, v.mRetainCount);
         assertEquals(1, mTestMath.getVariablePoolSize());
-        v = mTestMath.variable(1);
-        assertEquals(0, v.mRetainCount);
+        v = mTestMath.variable(1).retain();
+        assertEquals(1, v.mRetainCount);
         assertEquals(0, mTestMath.getVariablePoolSize());
     }
     
     public void testValueWrapperCreation() {
-        LayoutMath.ValueWrapper v = mTestMath.wrap();
+        LayoutMath.ValueWrapper v = mTestMath.wrap().retain();
         assertEquals(1, v.mRetainCount);
         assertEquals(0, mTestMath.getValueWrapperPoolSize());
         v.release();
         assertEquals(0, v.mRetainCount);
         assertEquals(1, mTestMath.getValueWrapperPoolSize());
-        v = mTestMath.wrap();
+        v = mTestMath.wrap().retain();
         assertEquals(1, v.mRetainCount);
         assertEquals(0, mTestMath.getValueWrapperPoolSize());
     }
     
     public void testBinaryOperationValueCreation() {
-        LayoutMath.BinaryOperationValue v = mTestMath.binaryOperation('+', mTestMath.unknown(), mTestMath.unknown());
-        assertEquals(0, v.mRetainCount);
+        LayoutMath.BinaryOperationValue v = mTestMath.binaryOperation('+', mTestMath.unknown(), mTestMath.unknown()).retain();
+        assertEquals(1, v.mRetainCount);
         assertEquals(0, mTestMath.getBinaryOperationPoolSize());
-        v.mRetainCount++;
         v.release();
         assertEquals(0, v.mRetainCount);
         assertEquals(1, mTestMath.getBinaryOperationPoolSize());
-        v = mTestMath.binaryOperation('+', mTestMath.unknown(), mTestMath.unknown());
-        assertEquals(0, v.mRetainCount);
+        v = mTestMath.binaryOperation('+', mTestMath.unknown(), mTestMath.unknown()).retain();
+        assertEquals(1, v.mRetainCount);
         assertEquals(0, mTestMath.getBinaryOperationPoolSize());
     }
     
     public void testWrapperWrapping() {
         LayoutMath.Value v = mTestMath.variable(10);
         LayoutMath.ValueWrapper wrapper1 = mTestMath.wrap();
-        LayoutMath.ValueWrapper wrapper2 = mTestMath.wrap();
+        LayoutMath.ValueWrapper wrapper2 = mTestMath.wrap().retain();
         wrapper1.setValueObject(v);
         wrapper2.setValueObject(wrapper1);
-        wrapper1.release();
         wrapper2.release();
-        v.release();
         assertEquals(1, mTestMath.getVariablePoolSize());
         assertEquals(2, mTestMath.getValueWrapperPoolSize());
     }
     
     public void testValueWrapping() {
         LayoutMath.Value v = mTestMath.variable(10);
-        LayoutMath.ValueWrapper wrapper = mTestMath.wrap();
+        LayoutMath.ValueWrapper wrapper = mTestMath.wrap().retain();
         wrapper.setValueObject(v);
         wrapper.release();
-        v.release();
         assertEquals(1, mTestMath.getVariablePoolSize());
         assertEquals(1, mTestMath.getValueWrapperPoolSize());
     }
@@ -77,7 +72,7 @@ public class LayoutMathTest extends TestCase {
         LayoutMath.Value x1 = mTestMath.variable(10);
         LayoutMath.Value x2 = mTestMath.variable(10);
         LayoutMath.Value w = mTestMath.variable(64);
-        LayoutMath.Value y = mTestMath.wrap(x1.subtract(x2).subtract(w));
+        LayoutMath.Value y = mTestMath.wrap(x1.subtract(x2).subtract(w)).retain();
         y.release();
         assertEquals(3, mTestMath.getVariablePoolSize());
         assertEquals(2, mTestMath.getBinaryOperationPoolSize());
@@ -89,9 +84,8 @@ public class LayoutMathTest extends TestCase {
         LayoutMath.Value x2 = mTestMath.variable(320);
         LayoutMath.Value w1 = mTestMath.variable(64);
         LayoutMath.Value w2 = mTestMath.variable(64);
-        LayoutMath.ValueWrapper wrapper = mTestMath.wrap();
-        LayoutMath.Value sum = x2.subtract(x1).subtract(w1).subtract(w2);
-        wrapper.setValueObject(sum);
+        LayoutMath.ValueWrapper wrapper = mTestMath.wrap().retain();
+        wrapper.setValueObject(x2.subtract(x1).subtract(w1).subtract(w2));
         wrapper.release();
         assertEquals(4, mTestMath.getVariablePoolSize());
         assertEquals(1, mTestMath.getValueWrapperPoolSize());
@@ -106,14 +100,8 @@ public class LayoutMathTest extends TestCase {
         LayoutMath.ValueWrapper w2 = mTestMath.wrap();
         w2.setValueObject(x2);
         LayoutMath.Value sum = w1.add(w2);
-        LayoutMath.ValueWrapper wrapper = mTestMath.wrap();
-        wrapper.setValueObject(sum);
+        LayoutMath.ValueWrapper wrapper = mTestMath.wrap(sum).retain();
         wrapper.release();
-        sum.release();
-        w1.release();
-        w2.release();
-        x1.release();
-        x2.release();
         assertEquals(2, mTestMath.getVariablePoolSize());
         assertEquals(3, mTestMath.getValueWrapperPoolSize());
         assertEquals(1, mTestMath.getBinaryOperationPoolSize());
